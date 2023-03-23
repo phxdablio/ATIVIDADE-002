54a3c510c689acc12a146e09068d89f903768fe5
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2023-03-23 11:20:50 -0300

    Add files via upload

---
+++ ex 01.c
@@ -0,0 +1,22 @@
+/* 1.Faça um programa que leia dois números e mostre qual deles é o maior.*/
+
+int main ()
+{
+	
+	float n1, n2;
+	
+	printf("Digite um numero: ");
+	scanf("%f",&n1);
+	
+	printf("Digite outro numero: ");
+	scanf("%f",&n2);
+	
+	if (n1 > n2){
+		
+	printf ("%f é maior que %0.f",n1,n2);
+	}
+	else {
+	printf	("%f é maior que %0.f",n2,n1);
+	}
+}
+
